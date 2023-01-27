fdfedb2e70c3b498566664c1ffa545aca1598933
Kristina Trifunovic <kristina.trifunovic@eng.it>
2023-01-27 21:26:44 +0100

    installed font awesome and changed scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,9 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon'
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -37,7 +36,7 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatSidenavModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {faInstagram, faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'home',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  linkedIn = faLinkedin;
+  gitHub = faGithub;
+  email = faEnvelope;
+  instagram = faInstagram;
 }
