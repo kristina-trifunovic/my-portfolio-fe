b352b715cf059e4009b8bae8b020fc435350a3ea
Kristina Trifunovic <kristina.trifunovic@eng.it>
2023-02-04 17:57:47 +0100

    project setup

---
+++ src/app/model/project.model.ts
@@ -0,0 +1,5 @@
+export interface Project {
+  name: string;
+  git: string;
+  description: string;
+}
--- src/app/view/projects/projects.component.ts
+++ src/app/view/projects/projects.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/model/project.model';
+import { ProjectsService } from 'src/app/service/projects.service';
 
 @Component({
   selector: 'projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  projects: Project[] = [];
 
+  constructor(private projectsService: ProjectsService) {}
+
+  ngOnInit(): void {
+    this.loadProjects();
+  }
+
+  loadProjects() {
+    this.projectsService.loadProjects().subscribe((data: any) => this.projects = data.list);
+  }
 }
